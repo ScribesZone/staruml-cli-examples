82b84610732f396e57354b7af95dd2045968b883
escribis <escribis@users.noreply.github.com>
2022-08-12 09:40:38 +0200

    today

---
+++ staruml/types/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="all.d.ts" />
+/// <reference path="core.d.ts" />
+/// <reference path="erd.d.ts" />
+/// <reference path="uml.d.ts" />
+/// <reference path="graphics.d.ts" />
+/// <reference path="metameta.d.ts" />
